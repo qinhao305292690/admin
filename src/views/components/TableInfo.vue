05bf010e99e68cd1d22b27f7dc6394f25c4b29af
18310957475 <18310957475@163.com>
2020-05-27 16:53:35 +0800

    提交新代码

--- src/views/components/TableInfo.vue
+++ src/views/components/TableInfo.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <a-table
-         bordered
-         rowKey="title"
-         :columns="columns"
-         :data-source="tableData">
+                bordered
+                rowKey="title"
+                :columns="columns"
+                :data-source="payload">
+            <template #aCode="text,row,index">
+                <a-input @input="(e)=>handleInput(e,'aCode',index,row)" placeholder="请输入" :value="text"/>
+            </template>
             <template #code="text,row,index">
-                <a-input @input="(e)=>handleInput(e,'code',index)" placeholder="请输入" :value="text"/>
+                <a-input @input="(e)=>handleInput(e,'code',index,row)" placeholder="请输入" :value="text"/>
             </template>
             <template #price="text,row,index">
-                <a-input @input="(e)=>handleInput(e,'price',index)" placeholder="请输入" :value="text"/>
+                <a-input @input="(e)=>handleInput(e,'price',index,row)" placeholder="请输入" :value="text"/>
             </template>
         </a-table>
     </div>
@@ -17,40 +20,63 @@
 
 <script>
     export default {
-        props:{
-            tableData:{
+        props: {
+            tableData: {
                 type: Array
             }
         },
         methods: {
-            handleInput({target},keys,index){
-                console.log(target.value,index);
+            handleInput({target}, keys, index,recods) {
                 let val = target.value
                 let result = [...this.tableData];
-                result[index][keys] = val;
-                this.$emit('update:tableData',result)
+                result.forEach(item=>{
+                    item.czg.forEach(row=>{
+                        if(row.title == recods.title){
+                            console.log(keys);
+                            row[keys] = val
+                        }
+                    })
+                })
+                this.$emit('update:tableData', result)
             }
         },
         name: "TableInfo",
         data() {
             return {
-                columns:[
-                    {
-                        title: '规格',
-                        dataIndex: 'title',
-                    },
-                    {
-                        title: '规格编码',
-                        dataIndex: 'code',
-                        scopedSlots:{customRender:'code'}
-                    },
-                    {
-                        title: '价格',
-                        dataIndex: 'price',
-                        scopedSlots:{customRender:'price'}
-                    },
-                ],
+                columns: [],
                 payload: []
+            }
+        },
+        watch: {
+            tableData: {
+                handler: function (arr) {
+                    this.columns = arr.map(item => ({
+                        title: item.specValue == 'colorList' ? '颜色' : item.specValue == 'sizeList' ? '尺寸' : '尺码',
+                        dataIndex: 'title',
+                    }))
+                    this.columns = [
+                        ...this.columns,
+                        {
+                            title: '规格编码',
+                            dataIndex: 'aCode',
+                            scopedSlots: {customRender: 'aCode'}
+                        },
+                        {
+                            title: '规格条码',
+                            dataIndex: 'code',
+                            scopedSlots: {customRender: 'code'}
+                        },
+                        {
+                            title: '建议价格',
+                            dataIndex: 'price',
+                            scopedSlots: {customRender: 'price'}
+                        }]
+                    for (const Aval of arr) {
+                        this.payload = Aval.czg.map(item => Object.assign(item))
+                    }
+
+                },
+                deep: true
             }
         }
     }
--- src/views/page/WelcomePage.vue
+++ src/views/page/WelcomePage.vue
@@ -1,79 +1,100 @@
 <template>
     <div class="container">
-        <section class="wrap">
-            <span>
-               规格：
-            </span>
-            <a-select v-model="specValue" style="width: 150px">
-                <a-select-option :value="item.target" :key="item.target" v-for="item of specificationsList">
-                    {{item.title}}
-                </a-select-option>
-            </a-select>
-        </section>
-        <br>
-        <!--颜色:-->
-        <aside class="tag-container">
-            <a-tag @close="() => handleClose(i.title,i.target)" :key="i.title" v-for="(i,index) in czg" closable
-                   :visible="true" color="cyan">
-                {{i.title}}
-            </a-tag>
-
-            <a-popover title="请选择" trigger="click" placement="bottom">
-                <template slot="content">
+        <div class="padding" :key="index" v-for="(item,index) of specis">
+            <a-button @click="hanleRemoveItem(index)" type="link" style="float:right;cursor: pointer"
+                      icon="close-circle"></a-button>
+            <section class="wrap">
                 <span>
-                   规格参数：
+                   规格设置：
                 </span>
-                    <a-select
-                            v-if="specValue=='colorList'"
-                            autoClearSearchValue
-                            v-model="c"
-                            mode="multiple"
-                            style="width: 150px"
-                            placeholder="请选择"
-                            @change="(e)=>handleChange(e,'c')">
-                        <a-select-option :key="item.value" v-for="(item) in colorList" :value="item.title">
-                            {{item.title}}
-                        </a-select-option>
-                    </a-select>
-                    <a-select
-                            v-model="z"
-                            v-if="specValue=='sizeList'"
-                            mode="multiple"
-                            style="width: 150px"
-                            placeholder="请选择"
-                            @change="(e)=>handleChange(e,'z')">
-                        <a-select-option :key="item.value" v-for="(item) in sizeList" :value="item.title">
-                            {{item.title}}
-                        </a-select-option>
-                    </a-select>
-                    <a-select
-                            v-model="g"
-                            v-if="specValue=='ggList'"
-                            mode="multiple"
-                            style="width: 150px"
-                            placeholder="请选择"
-                            @change="(e)=>handleChange(e,'g')">
-                        <a-select-option :key="item.value" v-for="(item) in ggList" :value="item.title">
-                            {{item.title}}
-                        </a-select-option>
-                    </a-select>
-                </template>
-                <a-button type="link" icon="plus" style="margin-top: 10px;">添加</a-button>
-            </a-popover>
-        </aside>
+                <a-select
+                        @change="handleHasItem(index)"
+                        v-model="item.specValue"
+                        style="width: 150px">
+                    <a-select-option
+                            :value="item.target"
+                            :key="item.target"
+                            v-for="item of specificationsList">
+                        {{item.title}}
+                    </a-select-option>
+                </a-select>
+            </section>
+            <br>
+            <!--颜色:-->
+            <aside class="tag-container">
+                <a-tag @close="() => handleClose(i.title,i.target,index)" :key="i.title" v-for="(i) in item.czg"
+                       closable
+                       :visible="true" color="cyan">
+                    {{i.title}}
+                </a-tag>
 
-        <a-button style="margin-top: 10px;" type="primary">添加规格</a-button>
+                <a-popover :destroyTooltipOnHide="!has_specis" title="请选择" trigger="click" placement="bottom">
+                    <template slot="content">
+                    <span>
+                       规格参数：
+                    </span>
+                        <a-select
+                                v-if="item.specValue=='colorList'"
+                                autoClearSearchValue
+                                v-model="item.c"
+                                mode="multiple"
+                                style="width: 150px"
+                                placeholder="请选择"
+                                @change="(e)=>handleChange(e,'c',index)">
+                            <a-select-option :key="r.value" v-for="(r) in colorList" :value="r.title">
+                                {{r.title}}
+                            </a-select-option>
+                        </a-select>
+                        <a-select
+                                v-model="item.z"
+                                v-if="item.specValue=='sizeList'"
+                                mode="multiple"
+                                style="width: 150px"
+                                placeholder="请选择"
+                                @change="(e)=>handleChange(e,'z',index)">
+                            <a-select-option :key="r.value" v-for="(r) in sizeList" :value="r.title">
+                                {{r.title}}
+                            </a-select-option>
+                        </a-select>
+                        <a-select
+                                v-model="item.g"
+                                v-if="item.specValue=='ggList'"
+                                mode="multiple"
+                                style="width: 150px"
+                                placeholder="请选择"
+                                @change="(e)=>handleChange(e,'g',index)">
+                            <a-select-option :key="r.value" v-for="(r) in ggList" :value="r.title">
+                                {{r.title}}
+                            </a-select-option>
+                        </a-select>
+                    </template>
+                    <a-button :disabled="has_specis" type="link" icon="plus" style="margin-top: 10px;">添加</a-button>
+                </a-popover>
+            </aside>
+        </div>
+        <a-button :disabled="specis.length>=3" @click="handleAddspecis" style="margin-top: 10px;" type="primary">添加规格
+        </a-button>
         <section style="margin-top: 10px;background: #fff;">
             <p style="margin: 10px 0 10px 0;">规格信息:</p>
-
             <table-info
-                    :tableData.sync="czg"
-            ></table-info>
+             :tableData.sync="specis"
+            />
         </section>
     </div>
 </template>
 <script>
     import TableInfo from '@/views/components/TableInfo'
+
+    /**
+     *  @@ 默认添加的对象
+     * **/
+    const specisChildrenObj = {
+        specValue: '',
+        c: [],
+        z: [],
+        g: [],
+        czg: []
+    }
 
     export default {
         name: "WelcomePage",
@@ -82,14 +103,9 @@
         },
         data() {
             return {
-                specis: [
-                    
-                ],
-                specValue: 'colorList',
-                c: [],
-                z: [],
-                g: [],
-                czg: [],
+                has_specis: false,
+                // TODO: 这是每一项数据
+                specis: [{...specisChildrenObj}],
                 specificationsList: [
                     {
                         title: '颜色',
@@ -140,21 +156,46 @@
 
         },
         methods: {
-            handleChange(valueArr,target) {
-                this[target] = valueArr
-                this.czg = this.mapArray()
+            handleAddspecis() {
+                console.log(specisChildrenObj, 'aaa');
+                this.specis.push({...specisChildrenObj})
             },
-            mapArray(){
+            hanleRemoveItem(index) {
+                this.specis.splice(index, 1)
+            },
+            handleHasItem(index) {
+                let flag = false
+                for (let i = 0, length = this.specis.length; i < length; i++) {
+                    if (i === index) continue;
+                    if (this.specis[i].specValue === this.specis[index].specValue) {
+                        flag = true
+                        this.$message.warning('规格名不能相同,请更换')
+                    }
+                }
+                this.has_specis = flag
+
+            },
+            handleChange(valueArr, target, index) {
+                this.specis[index][target] = valueArr
+                this.specis[index].czg = this.mapArray(index)
+            },
+            mapArray(index) {
+                // if()
                 return [
-                    ...this.c.map(item=>({title:item,target:'c',code: '0',price: '0'})),
-                    ...this.z.map(item=>({title:item,target:'z',code: '0',price: '0'})),
-                    ...this.g.map(item=>({title:item,target:'g',code: '0',price: '0'}))
+                    ...this.specis[index].c.map((item,i) =>
+                        (Object.assign({
+                            title: item, target: 'c',aCode:'', code: '0', price: '0'
+                            },
+                            this.specis[index].czg[i]
+                        ))
+                    ),
+                    ...this.specis[index].z.map(item => ({title: item, target: 'z',aCode:'', code: '0', price: '0'})),
+                    ...this.specis[index].g.map(item => ({title: item, target: 'g',aCode:'', code: '0', price: '0'}))
                 ]
             },
-            handleClose(title, target) {
-                this[target] = this[target].filter(ArrTitle=>ArrTitle!==title)
-                console.log(this[target],'thiss');
-                this.czg = this.czg.filter(item=>item.title!==title)
+            handleClose(title, target, index) {
+                this.specis[index][target] = this.specis[index][target].filter(ArrTitle => ArrTitle !== title)
+                this.specis[index].czg = this.specis[index].czg.filter(item => item.title !== title)
             }
         },
     }
@@ -178,5 +219,12 @@
         margin-top: 10px;
         border-radius: 5px;
         padding: 6px;
+    }
+
+    .padding {
+        border: 1px solid #eeecf7;
+        padding: 10px;
+        border-radius: 5px;
+        margin-top: 10px;
     }
 </style>
