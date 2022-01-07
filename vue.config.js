6b16690aed4c6e0e1e0b2687b173db8089814338
qinhaojoyc <18310957475@163.com>
2022-01-07 22:18:23 +0800

    新增可视化

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
 module.exports = {
   lintOnSave: false,
+  productionSourceMap: false,  // 生产环境的 source map
+  chainWebpack: config => config.output.filename('[name].[hash:16].js').end(),
+  configureWebpack: config => {
+    config.entry.app = ["babel-polyfill", "./src/main.js"];
+  },
+  publicPath: '/static/dist',
   //抛出全局变量
   pluginOptions: {
     'style-resources-loader': {
